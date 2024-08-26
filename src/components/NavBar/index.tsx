e5032ed618711ec3950e02d2342a1f95d5d2a036
Guilherme <guilhermezapas@gmail.com>
2024-08-25 21:41:02 -0300

    adding prettier-plugin-taiwindcss

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div style={{backgroundColor: 'red'}} className='bg-blue-950 border-b border-blue-500 p-10 '>
-
-    <h1>teste</h1>
-    <h2>teste</h2>
-    <p>teste</p>
+    <div className="border-b border-blue-500 bg-blue-950 p-10">
+      <h1>teste</h1>
+      <h2>teste</h2>
+      <p>teste</p>
     </div>
   )
 }
