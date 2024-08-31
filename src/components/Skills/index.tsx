7a5f01bdecbdddde78d045e479ff9a4c2697e135
Guilherme <guilhermezapas@gmail.com>
2024-08-31 14:07:52 -0300

    adding titles inputs and images at skills component

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -14,6 +14,8 @@ import nextjs from '../../../public/assets/images/nextjs.svg'
 import styledComponents from '../../../public/assets/images/styled-components.svg'
 import typescript from '../../../public/assets/images/typescript.svg'
 import materialUI from '../../../public/assets/images/material-ui.svg'
+import postgreSQL from '../../../public/assets/images/postgresql.svg'
+import jira from '../../../public/assets/images/jira.svg'
 import Image from 'next/image'
 
 const Skills = () => {
@@ -30,38 +32,48 @@ const Skills = () => {
       <div className="mt-10">
         <p className="text-white">Linguagens e Ferramentas</p>
         <div className="mt-4 grid w-full grid-cols-[repeat(auto-fit,minmax(50px,50px))] justify-center gap-8">
-          <Image src={html5} alt="HTML5" />
-          <Image src={css3} alt="CSS3" />
-          <Image src={js} alt="JavaScript" />
-          <Image src={typescript} alt="TypeScript" />
-          <Image src={nodejs} alt="Node.js" />
-          <Image src={vite} alt="Vite" />
-          <Image src={figma} alt="Figma" />
+          <Image title="HTML5" src={html5} alt="HTML5" />
+          <Image title="CSS3" src={css3} alt="CSS3" />
+          <Image title="JavaScript" src={js} alt="JavaScript" />
+          <Image title="TypeScript" src={typescript} alt="TypeScript" />
+          <Image title="Node.js" src={nodejs} alt="Node.js" />
+          <Image title="Vite" src={vite} alt="Vite" />
+          <Image title="Figma" src={figma} alt="Figma" />
+          <Image title="Jira" src={jira} alt="Jira" />
         </div>
       </div>
       <div className="mt-10">
         <p className="text-white">Bibliotecas e Frameworks</p>
         <div className="mt-4 grid w-full grid-cols-[repeat(auto-fit,minmax(50px,50px))] justify-center gap-8">
-          <Image src={react} alt="React.js" />
-          <Image src={nextjs} alt="Next.js" />
-          <Image src={tailwind} alt="Tailwind" />
-          <Image src={styledComponents} alt="Styled-components" />
-          <Image src={materialUI} alt="MaterialUI" />
+          <Image title="React.js" src={react} alt="React.js" />
+          <Image title="Next.js" src={nextjs} alt="Next.js" />
+          <Image title="Tailwind" src={tailwind} alt="Tailwind" />
+          <Image
+            title="Styled-components"
+            src={styledComponents}
+            alt="Styled-components"
+          />
+          <Image title="Material UI" src={materialUI} alt="Material UI" />
         </div>
       </div>
-      <div className="flex gap-20">
-        <div className="mt-10">
-          <p className="text-white">Banco de dados</p>
-          <div className="mt-4 grid w-full grid-cols-[repeat(auto-fit,minmax(50px,50px))] justify-center gap-8">
-            <Image src={mongoDB} alt="MongoDB" />
-          </div>
+
+      <div className="mt-10">
+        <p className="text-white">Banco de dados</p>
+        <div className="mt-4 grid w-full grid-cols-[repeat(auto-fit,minmax(50px,50px))] justify-center gap-8">
+          <Image title="PostgreSQL" src={postgreSQL} alt="PostgreSQL" />
+          <Image title="MongoDB" src={mongoDB} alt="MongoDB" />
+          <div></div>
+          <div></div>
         </div>
-        <div className="mt-10 flex-1">
-          <p className="text-white">Outros</p>
-          <div className="mt-4 grid w-full grid-cols-[repeat(auto-fit,minmax(50px,50px))] justify-center gap-8">
-            <Image src={git} alt="Git" />
-            <Image src={gitHub} alt="GitHub" />
-          </div>
+      </div>
+
+      <div className="mt-10 flex-1">
+        <p className="text-white">Outros</p>
+        <div className="mt-4 grid w-full grid-cols-[repeat(auto-fit,minmax(50px,50px))] justify-center gap-6">
+          <Image title="Git" src={git} alt="Git" />
+          <Image title="GitHub" src={gitHub} alt="GitHub" />
+          <div></div>
+          <div></div>
         </div>
       </div>
     </div>
